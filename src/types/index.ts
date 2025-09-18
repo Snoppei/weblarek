052599e5fd372360cc3f9101bd966190b9cb68c9
Snoppei <negulyaev2017@gmail.com>
2025-09-18 09:22:45 +0300

    add: types and interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,38 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
-    get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+  get<T extends object>(uri: string): Promise<T>;
+  post<T extends object>(
+    uri: string,
+    data: object,
+    method?: ApiPostMethods
+  ): Promise<T>;
+}
+
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+
+export type TPayment = "card" | "cash" | "";
+
+export interface IBuyer {
+  payment: TPayment;
+  email: string;
+  phone: string;
+  address: string;
+}
+
+export interface IOrderPostRequest extends IBuyer {
+  total: number;
+  items: string[];
+}
+
+export interface IProductsResponse {
+  total: number;
+  items: IProduct[];
 }
