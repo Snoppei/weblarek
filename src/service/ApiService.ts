aea1b00fc2c5dcbe0466807a3e0bd152e56442c5
Snoppei <negulyaev2017@gmail.com>
2025-09-18 09:22:22 +0300

    add: apiservice

---
+++ src/service/ApiService.ts
@@ -0,0 +1,42 @@
+import { Api } from "../components/base/Api";
+import { IProduct, IBuyer, IOrderPostRequest } from "../types";
+
+export class ApiService {
+  private api: Api;
+
+  constructor(apiInstance: Api) {
+    this.api = apiInstance;
+  }
+
+  fetchProducts(): Promise<IProduct[]> {
+    return this.api
+      .get<{ total: number; items: IProduct[] }>("/product")
+      .then((response) => response.items);
+  }
+
+    fetchProductById(productId: string): Promise<IProduct> {
+      return this.api.get<IProduct>(`/product/${productId}`);
+    }
+
+  sendOrder(buyer: IBuyer, items: IProduct[]): Promise<any> {
+    let total = 0;
+    for (const item of items) {
+      if (item.price !== null) {
+        total += item.price;
+      }
+    }
+
+    const itemsIds = items.map((item) => item.id);
+
+    const orderData: IOrderPostRequest = {
+      payment: buyer.payment,
+      email: buyer.email,
+      phone: buyer.phone,
+      address: buyer.address,
+      total,
+      items: itemsIds,
+    };
+
+    return this.api.post("/order", orderData);
+  }
+}
